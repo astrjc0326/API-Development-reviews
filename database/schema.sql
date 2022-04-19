06816bc8133e7f6320bce0d728b244e70ea4d408
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-19 10:45:49 -0700

    name of primary database

--- database/schema.sql
+++ database/schema.sql
@@ -5,9 +5,23 @@ CREATE DATABASE review;
 USE review;
 
 CREATE TABLE reviews (
-  id int NOT NULL AUTO_INCREMENT,
+  id integer NOT NULL AUTO_INCREMENT,
+  rating integer NOT NULL,
   summary varchar(255) NOT NULL,
   body varchar(1024) NOT NULL,
-  -- photos
+  email varchar(255) NOT NULL,
+  username varchar(60) NOT NULL,
+  reported BOOLEAN default 0,
+  helpfulness integer default 0
+);
+
+-- CREATE TABLE meta(
+
+-- )
+
+CREATE TABLE photos (
+  id integer NOT NULL AUTO_INCREMENT,
+  review_id integer,
+  url text[][][][][]
 )
 
