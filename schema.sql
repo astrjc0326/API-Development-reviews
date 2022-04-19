48ffe282efcf2f0c0996973dcf8f944f8fd17787
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-19 09:25:40 -0700

    set up the database

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS review;
+
+CREATE DATABASE review;
+
+USE review;
+
+CREATE TABLE reviews (
+  id int NOT NULL AUTO_INCREMENT,
+  summary varchar(255) NOT NULL,
+  body varchar(1024) NOT NULL,
+  photos
+)
+
