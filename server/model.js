296c13c1178afc64daab05273d8acc302dd8c8f0
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-19 16:09:05 -0700

    set up routes

--- server/model.js
+++ server/model.js
@@ -5,15 +5,15 @@ module.exports = {
     res.send(`reviews for ${req.query.product_id}`);
   },
   getMeta: async (req, res) => {
-
+    res.send(`reviews meta for ${req.query.product_id}`);
   },
   postReview: async (req, res) => {
-
+    res.send(`post a review for productID: ${req.body.product_id}`);
   },
-  putReviewHelpful: async(req, res) => {
-
+  putReviewHelpful: async (req, res) => {
+    res.send('put a review helpful');
   },
-  reportReview: async(req, res) => {
-
+  reportReview: async (req, res) => {
+    res.send('report a reviews');
   },
 };
