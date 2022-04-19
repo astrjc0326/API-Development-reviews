720e6f8f1803eb1b489a3a450bc078b0e00c974b
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-19 16:08:39 -0700

    adding recommended

--- database/mongodb.js
+++ database/mongodb.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://githubfetch:hackreactor@cluster0.ndqjv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true });
 
 const reviewSchema = mongoose.Schema({
+  product_id: Number,
   username: String,
   email: String,
   summary: String,
   body: String,
   reported: Boolean,
   helpfulness: Number,
+  recommended: Boolean,
   photos: [String],
   rating: Number,
 });
