583903aa36d97a7ef5f25a4fd407b339e93bf69a
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-19 16:10:06 -0700

    set up mongodb

--- server/model.js
+++ server/model.js
@@ -1,5 +1,5 @@
 const db = require('../database');
-
+// const db = require('../database/mongodb');
 module.exports = {
   getReview: async (req, res) => {
     res.send(`reviews for ${req.query.product_id}`);
