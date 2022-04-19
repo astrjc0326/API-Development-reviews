9526913a2289f0e559d8dd557c4897755a380651
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-19 10:17:55 -0700

    eslint

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
-const router = require('./routes');
-
-const PORT = 3000 || env.process.PORT;
+const PORT = 3000 || process.env.PORT;
 
 app.use(express.static('client/dist'));
 app.use(express.json());
-// app.use('', router);
 
-app.get('', (req, res) => {
-  res.send('hello world')
-})
-
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+app.get('/reviews', (req, res) => {
+  res.send(`reviews for ${req.query.product_id}`);
+});
+app.get('/reviews/meta');
+app.post('/reviews');
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
