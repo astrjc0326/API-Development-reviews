4ce83fd6f63f1b5f220edfe98b68de479289e1e4
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-21 10:28:11 -0700

    setup the index

--- db/schema.sql
+++ db/schema.sql
@@ -21,11 +21,16 @@ CREATE TABLE reviews (
   helpfulness INT DEFAULT 0
 );
 
+CREATE INDEX idx_r_product_id ON reviews(product_id);
+
+
 CREATE TABLE characteristics(
   id BIGSERIAL PRIMARY KEY,
   product_id INT,
   name VARCHAR(50)
 );
+
+CREATE INDEX idx_char_product_id ON characteristics(product_id);
 
 CREATE TABLE characteristic_reviews(
   id BIGSERIAL PRIMARY KEY,
@@ -34,12 +39,15 @@ CREATE TABLE characteristic_reviews(
   value INT
 );
 
+CREATE INDEX idx_char_rev_product_id ON characteristic_reviews(review_id);
+
 CREATE TABLE photos (
   id BIGSERIAL PRIMARY KEY,
   review_id INT,
   url VARCHAR(255)
 );
 
+CREATE INDEX idx_photo_reviews_id ON photos(review_id);
 
 -- Define foreign keys
 -- ALTER TABLE meta ADD CONSTRAINT FK_Meta_Reviews FOREIGN KEY (product_id) REFERENCES reviews (product_id);
@@ -47,6 +55,8 @@ CREATE TABLE photos (
 -- ALTER TABLE characteristics ADD CONSTRAINT FK_Recommendation_Meta FOREIGN KEY (product_id) REFERENCES meta (product_id);
 -- ALTER TABLE photos ADD CONSTRAINT FK_Photo_Reviews FOREIGN KEY (product_id) REFERENCES reviews (product_id);
 
--- index
+
 
 -- psql -p5433 "review"
+-- index
+
