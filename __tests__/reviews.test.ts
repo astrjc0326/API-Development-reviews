c847f280cf4c6b1e15a9c04b0cf6046e57b82e60
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-22 08:57:28 -0700

    add the first test

---
+++ __tests__/reviews.test.ts
@@ -0,0 +1,12 @@
+describe('reviews', () => {
+
+  describe('get reviews route', () => {
+
+    describe('given the product does not exist', () => {
+
+      it ('should return a 404', () => {
+        expect(true).toBe(true);
+      })
+    })
+  })
+})
