af729c6cfdf343629c5ace4a8df0ec0b3a28958a
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-22 08:41:23 -0700

    set up server and query

--- server/controllers.js
+++ server/controllers.js
@@ -3,14 +3,22 @@ const models = require('./models');
 module.exports = {
   getReview: (req, res) => {
     const productID = req.query.product_id;
+    const page = req.query.page || 1;
+    const count = req.query.count || 5;
     models.getReview(productID, (err, result) => {
-      res.send(result);
+      if (!result) { res.send(501); }
+      const data = {};
+      data.product_id = productID;
+      data.page = page;
+      data.count = count;
+      data.results = result[0].results;
+      res.send(data);
     });
   },
   getMeta: (req, res) => {
     const productID = req.query.product_id;
     models.getMeta(productID, (err, result) => {
-      res.send(result);
+      res.send(result[0].results);
     });
   },
   postReview: (req, res) => {
@@ -19,7 +27,7 @@ module.exports = {
     console.log('post a review');
     const currDate = new Date();
     const data = [
-      currDate,
+      122,
       review.product_id,
       review.rating,
       review.summary,
@@ -29,10 +37,10 @@ module.exports = {
       review.recommend,
       review.helpfulness,
     ];
-    models.postReview(data, (err) => {
+    models.postReview(data, req.body.photos, req.body.characteristics, (err) => {
       if (!err) {
         console.log('post a reviews successfully');
-        res.sendStatus(201);
+        res.status(201);
       }
     });
   },
--- server/models.js
+++ server/models.js
@@ -3,28 +3,85 @@ const db = require('../db');
 module.exports = {
   getReview: (productID, callback) => {
     const query = `
-    SELECT id, date, rating, summary, body, reviewer_name, recommend, helpfulness, response FROM reviews WHERE product_id = $1
+    SELECT
+    json_agg(
+      json_build_object(
+        'review_id', id,
+        'rating', rating,
+        'date', date,
+        'summary', summary,
+        'body', body,
+        'reviewer_name', reviewer_name,
+        'recommend', recommend,
+        'helpfulness', helpfulness,
+        'response', response,
+        'photos', (
+          SELECT coalesce(json_agg(url), '[]'::json)
+          FROM (
+            SELECT url
+            FROM photos
+            WHERE review_id = r.id
+          ) as photos
+        )
+      )
+    ) as results
+    FROM reviews r WHERE product_id = $1;
     `;
     db.pool.query(query, [productID], (err, result) => {
-      console.log(result);
-      callback(null, result.rows);
+      if (result) {
+        callback(null, result.rows);
+      }
+      else {
+        callback(null);
+      }
       console.log(err);
     });
   },
   getMeta: (productID, callback) => {
     const query = `
-    SELECT * FROM meta WHERE product_id = $1
+    SELECT json_build_object(
+      'product_id', product_id,
+       'ratings',
+           (SELECT json_object_agg(rating,num_reviews)
+              FROM (SELECT rating, count(*) as num_reviews from reviews
+                  WHERE product_id = $1 GROUP BY rating) r),
+       'recommended',
+            (SELECT json_object_agg(recommend,num_reviews)
+            FROM (SELECT recommend, count(*) as num_reviews FROM reviews
+             WHERE product_id = $1 group by recommend) re),
+      'characteristics',
+             (SELECT json_object_agg
+             ( name, json_build_object(
+                    'id', id,
+                    'value', value
+                 ))
+            FROM (
+              SELECT  c.name,  c.id, sum(value)/count(*) as value
+              FROM characteristics c
+              LEFT JOIN characteristic_reviews cr
+              ON c.id = cr.characteristic_id
+              WHERE c.product_id = $1
+              GROUP BY  c.name, c.id
+                ) r
+            )
+     ) as results
+    FROM reviews
+    WHERE product_id = $1 ;
     `;
     db.pool.query(query, [productID], (err, result) => {
       if (!err) {
-        callback(null, result);
+        callback(null, result.rows);
       }
     });
   },
-  postReview: (review, callback) => {
-    const query = 'INSERT INTO reviews (review_date, product_id, rating, summary, body, reviewer_email, reviewer_name, recommend, helpfulness) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);';
+  postReview: (review, photos, characteristics, callback) => {
+    const query = `
+    INSERT INTO reviews
+    (id, date, product_id, rating, summary, body, reviewer_email, reviewer_name, recommend, helpfulness) VALUES
+    (5774953, $1, $2, $3, $4, $5, $6, $7, $8, $9);`;
     db.pool.query(query, review, (err, result) => {
       if (!err) {
+        console.log(result);
         callback(null, result);
       } else {
         console.log(err);
@@ -35,10 +92,15 @@ module.exports = {
     ('2008-11-11', 1, 5, 'apple', 'apple',
     'apple', 'apple', true, false, 0, 'apple'); */
   },
-  putReviewHelpful: (req, res) => {
-    res.send('put a review helpful');
+  putReviewHelpful: (reviewID, callback) => {
+    const query = `
+    SELECT
+    `;
+    db.pool.query(query, reviewID, (err, result) => {
+
+    });
   },
-  reportReview: (req, res) => {
+  reportReview: (reviewID, callback) => {
     res.send('report a reviews');
   },
 };
