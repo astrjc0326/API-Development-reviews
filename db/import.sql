761ae47ea345fc95b7d472c41ef617e6ea0c2763
Hsin An Anny Wang <98345990+astrjc0326@users.noreply.github.com>
2022-04-22 08:41:05 -0700

    database insert information

--- db/import.sql
+++ db/import.sql
@@ -7,3 +7,34 @@
 --3347679 totals
 \copy characteristic_reviews FROM './csv/characteristic_reviews.csv' WITH (FORMAT CSV, HEADER);
 --19327575 totals
+
+-- need to not import the first column from the data
+
+    SELECT json_build_object(
+      'product_id', product_id,
+       'ratings',
+           (SELECT json_object_agg(rating,num_reviews)
+              FROM (SELECT rating, count(*) as num_reviews from reviews
+                  WHERE product_id = 1 GROUP BY rating) r),
+       'recommended',
+            (SELECT json_object_agg(recommend,num_reviews)
+            FROM (SELECT recommend, count(*) as num_reviews FROM reviews
+             WHERE product_id = 1 group by recommend) re),
+      'characteristics',
+             (SELECT json_object_agg
+             ( name, json_build_object(
+                    'id', id,
+                    'value', value
+                 ))
+            FROM (
+              SELECT  c.name,  c.id, sum(value)/count(*) as value
+              FROM characteristics c
+              LEFT JOIN characteristic_reviews cr
+              ON c.id = cr.characteristic_id
+              WHERE c.product_id = 1
+              GROUP BY  c.name, c.id
+                ) r
+            )
+     )
+    FROM reviews
+    WHERE product_id = 1 ;
--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE reviews (
   id BIGSERIAL PRIMARY KEY,
   product_id INT,
   rating INT NOT NULL,
-  date VARCHAR(13),
+  date BIGINT NOT NULL,
   summary VARCHAR(255) NOT NULL,
   body VARCHAR(1024) NOT NULL,
   recommend BOOLEAN,
@@ -34,12 +34,12 @@ CREATE INDEX idx_char_product_id ON characteristics(product_id);
 
 CREATE TABLE characteristic_reviews(
   id BIGSERIAL PRIMARY KEY,
-  product_id INT,
+  characteristic_id INT,
   review_id INT,
   value INT
 );
 
-CREATE INDEX idx_char_rev_product_id ON characteristic_reviews(review_id);
+CREATE INDEX idx_char_rev_characteristic_id ON characteristic_reviews(characteristic_id);
 
 CREATE TABLE photos (
   id BIGSERIAL PRIMARY KEY,
@@ -49,14 +49,37 @@ CREATE TABLE photos (
 
 CREATE INDEX idx_photo_reviews_id ON photos(review_id);
 
--- Define foreign keys
--- ALTER TABLE meta ADD CONSTRAINT FK_Meta_Reviews FOREIGN KEY (product_id) REFERENCES reviews (product_id);
--- ALTER TABLE recommendations ADD CONSTRAINT FK_Recommendation_Meta FOREIGN KEY (product_id) REFERENCES meta (product_id);
--- ALTER TABLE characteristics ADD CONSTRAINT FK_Recommendation_Meta FOREIGN KEY (product_id) REFERENCES meta (product_id);
--- ALTER TABLE photos ADD CONSTRAINT FK_Photo_Reviews FOREIGN KEY (product_id) REFERENCES reviews (product_id);
-
-
-
 -- psql -p5433 "review"
--- index
+    SELECT
+    json_agg(
+      json_build_object(
+        'review_id', id,
+        'rating', rating,
+        'date', date,
+        'summary', summary,
+        'body', body,
+        'reviewer_name', reviewer_name,
+        'recommend', recommend,
+        'helpfulness', helpfulness,
+        'response', response,
+        'photos', (
+          SELECT coalesce(json_agg(url), '[]'::json)
+          FROM (
+            SELECT url
+            FROM photos
+            WHERE review_id = r.id
+          ) as photos
+        )
+      )
+    ) as results
+    FROM reviews r WHERE product_id = 1;
 
+
+
+ SELECT AVG(value) FROM characteristic_reviews WHERE characteristic_id =1
+
+ update reviews set date = to_timestamp(date,'YYYY-DD-MM HH:MI');
+ALTER TABLE reviews
+ALTER date SET DATA TYPE timestamp without time zone
+USING timestamp without time zone 'epoch' + (date / 1000 ) * interval '1 second';
+update reviews set date=to_timestamp(date,'YYYY-DD-MON-RRHH24:MI:SS.FF');
